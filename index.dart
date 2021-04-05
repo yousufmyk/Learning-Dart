65ee0a011f01db4c8c4dc21f1bdfffa92cc55ce4
MUHAMMAD YOUSUF KHAN <yk445261@gmail.com>
2021-04-05 16:24:35 +0500

    code in dart

---
+++ index.dart
@@ -0,0 +1,65 @@
+void main () {
+
+// var NameAge = {1: 'Yousuf' ,2: 18 , 3: 'searching job'};
+  
+//   NameAge[5] = 'khan';
+  
+  
+//   var Nam = NameAge.putIfAbsent(4,() => 'Dubai');
+  
+  
+  
+//   print(NameAge);
+//   print(Nam);
+  
+  
+//   var AppData = {'Name':'Yousuf','Age':'18','Nationality':'Pakistani'};
+  
+//   print(AppData);
+  
+//   AppData.addAll({ 'Name':'khan','Age':'17','Nationality':'Pakistani'});
+  
+//   print(AppData);
+  
+  
+//   var StudentData = {321:['Yousuf',18,'Pakistani'] , 543:['Khan',20,'Pakistani']};
+  
+  
+//   print(StudentData);
+//   print(StudentData.keys);
+//   print(StudentData.values);
+  
+  
+//   int abc = 2;
+  
+//   print(++abc);
+  
+//   // answer ayega 3 ye pre increment hai
+  
+//   var cde = 5;
+  
+//    print( cde++);
+  
+//   //ab answer ayega 5 qk abi increment nhe hwa
+//   print(cde);
+  
+//   // ab answer ayega 6 ye post increment
+  
+//   //post increment me pehle real jo value hogi wo print hogi uske bad jo print hogi wo increase hpogi
+  
+//   var StudentData = {321:['Yousuf',18,'Pakistani'] , 543:['Khan',20,'Pakistani']};
+  
+//   StudentData.remove(543);
+  
+//   print(StudentData);
+  
+  
+//   var StudentData = {'Name':'Yousuf','Age':'18','Nationality':'Pakistani'};
+  
+  
+//   StudentData.removeWhere((key,value) => value.startsWith('Y') );
+  
+//   print(StudentData);
+  
+  
+  }
